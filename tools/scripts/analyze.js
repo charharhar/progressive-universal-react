6b2568848736287d11eb12ef5d8cb6d005a61c91
Charles Lee <charleslee90@gmail.com>
2017-04-06 17:27:47 +0800

    Add webpack bundle analyzer

---
+++ tools/scripts/analyze.js
@@ -0,0 +1,35 @@
+
+import webpack from 'webpack';
+import fs from 'fs';
+import { resolve as pathResolve } from 'path';
+import appRootDir from 'app-root-dir';
+import clientConfig from '../webpack/webpack.client';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import chalk from 'chalk';
+
+clientConfig.plugins.push(
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'server',
+    analyzerHost: '127.0.0.1',
+    analyzerPort: 8888,
+
+    reportFilename: 'report.html',
+    openAnalyzer: true,
+    // true => Webpack Stats JSON file will be in bundles output directory
+    generateStatsFile: true,
+    statsFilename: 'stats.json',
+
+    statsOptions: null,
+    logLevel: 'info',
+  })
+)
+
+const compiler = webpack(clientConfig);
+
+compiler.run((err, stats) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(chalk.bgYellow.black(`==> Running Webpack Bundle Analyzer.`))
+  }
+})
--- tools/webpack/webpack.client.js
+++ tools/webpack/webpack.client.js
@@ -19,6 +19,8 @@ const client = {
     vendor: [
       'react',
       'react-dom',
+      'react-router',
+      'react-router-dom',
     ],
   },
 
