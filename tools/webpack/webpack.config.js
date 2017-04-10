5fc6bb11c5d4ef7c2d040da1dd9eb3c28f496a7d
Charles Lee <charleslee90@gmail.com>
2017-04-10 18:19:59 +0800

    Add basic file-loader and url-loader support

--- tools/webpack/webpack.config.js
+++ tools/webpack/webpack.config.js
@@ -105,6 +105,21 @@ export default function configFactory(options) {
             }),
           ])
         },
+        {
+          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+          loader: 'file-loader',
+          query: {
+            name: isDev ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+          },
+        },
+        {
+          test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
+          loader: 'url-loader',
+          query: {
+            name: isDev ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+            limit: 10000,
+          },
+        },
       ]
     },
 
