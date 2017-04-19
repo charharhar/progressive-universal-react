d096c87cbacdcc7c54174f4abc34cf275f3c5aa3
Charles Lee <charleslee90@gmail.com>
2017-04-19 13:54:38 +0800

    Add very basic CSP security

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -1,7 +1,38 @@
 import helmet from 'helmet';
 import config from '../../tools/config';
 
+const cspConfig = {
+  directives: {
+    childSrc: ["'self'"],
+    connectSrc: ['*'],
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'"],
+    fontSrc: ["'self'"],
+    objectSrc: ["'self'"],
+    mediaSrc: ["'self'"],
+    manifestSrc: ["'self'"],
+    srciptSrc: [
+      "'self'",
+      "'unsafe-inline'",
+    ],
+    styleSrc: [
+      "'self'",
+      "'unsafe-inline'",
+      'blob:',
+    ]
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  Object.keys(cspConfig.directives).forEach((directive) => {
+    cspConfig.directives[directive].push(
+      `${config.host}:${config.clientPort}`,
+    );
+  })
+}
+
 const security = [
+  helmet.contentSecurityPolicy(cspConfig),
 
   helmet.xssFilter(),
 
