1186b7c9b264c49d88f63f6b5aa5ab14d8e706ef
Charles Lee <charleslee90@gmail.com>
2017-04-21 15:26:48 +0800

    Add global css values

--- shared/App.js
+++ shared/App.js
@@ -1,3 +1,4 @@
+import styles from './global.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from './config/routes';
@@ -13,7 +14,7 @@ import {
 } from './routes';
 
 const App = () => (
-  <div>
+  <div className={styles.wrapper}>
     <Navigation />
     <hr />
     <Switch>
