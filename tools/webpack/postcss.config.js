9319c92e91b0cad3ddef6512791f399b44b8ab5a
Charles Lee <charleslee90@gmail.com>
2017-06-07 12:12:22 +0800

    Updates more dependencies, not postcss loader yet

--- tools/webpack/postcss.config.js
+++ tools/webpack/postcss.config.js
@@ -5,7 +5,5 @@ module.exports = {
     require('autoprefixer'),
     // Include SASS like support in CSS files
     require('precss'),
-    // Fixes flex property bugs
-    require('postcss-flexbugs-fixes'),
   ]
 }
